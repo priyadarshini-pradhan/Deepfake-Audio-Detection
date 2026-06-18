674ad106ddc7d547312805019d780a5aec42ea7b
priyadarshini-pradhan <priyadarshinipradhan012@gmail.com>
2026-06-18 11:16:31 +0530

    fix keras conflict

--- app.py
+++ app.py
@@ -146,8 +146,5 @@ def predict():
 # Run App
 # -----------------------------
 if __name__ == "__main__":
-    app.run(
-        host="0.0.0.0",
-        port=5000,
-        debug=True
-    )
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
